6c96887fc1c0b4a91398a927c9713c17588218ee
rey <reyarby.castro@gmail.com>
2026-05-31 22:27:01 +0800

    added unit routes

---
+++ backend/src/routes/unit.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const { getAll, getById, create, update, remove } = require('../controllers/unit.controller');
+
+router.get('/',       auth, getAll);
+router.get('/:id',    auth, getById);
+router.post('/',      auth, create);
+router.put('/:id',    auth, update);
+router.delete('/:id', auth, remove);
+
+module.exports = router;
